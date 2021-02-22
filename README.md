804aedc1cdf8b948107860c93e6aba2e10fd8ab4
wildmolasses <changes@gmail.com>
2021-02-22 13:47:58 -0700

    Create scaffold

---
+++ README.md
@@ -0,0 +1,25 @@
+# Zeneth
+
+### Dependencies
+
+To ensure that everyone is using the same version of nodejs on this project, [volta](https://volta.sh) is recommended!
+
+### Develop
+
+```sh
+yarn
+yarn dev
+```
+
+### Test
+
+```sh
+yarn test
+```
+
+### Serve
+
+```sh
+yarn build
+yarn start
+```
---
+++ frontend/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: '../.eslintrc.js',
+  parserOptions: {
+    project: 'tsconfig.json',
+    parser: '@typescript-eslint/parser',
+    extraFileExtensions: ['.vue'],
+  },
+  globals: {
+    ga: true,
+    cordova: true,
+    __statics: true,
+    process: true,
+    Capacitor: true,
+    chrome: true,
+  },
+};
