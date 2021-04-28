ab91812c33e27f6749bdccad58448df88579c6dd
Ben DiFrancesco <ben@scopelift.co>
2021-04-28 06:51:29 -0400

    Implement Swap portion of swapAndBribe
    * Set up Uniswap router and WETH/Token pool in our
      tests
    * Implement and test swapping functionality

--- contracts/contracts/SwapBriber.sol
+++ contracts/contracts/SwapBriber.sol
@@ -2,14 +2,24 @@
 pragma solidity ^0.8.4;
 
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract SwapBriber {
 
     function swapAndBribe(
-        address _token,
+        IERC20 _token,
         uint256 _tokenAmount,
-        uint256 _ethBribe)
-    public {
-
+        uint256 _ethBribe,
+        IUniswapV2Router02 _router,
+        address[] calldata _path,
+        uint256 _deadline
+    ) public {
+        SafeERC20.safeTransferFrom(_token, msg.sender, address(this), _tokenAmount);
+        _token.approve(address(_router), _tokenAmount);
+        _router.swapExactTokensForETH(_tokenAmount, _ethBribe, _path, address(this), _deadline);
     }
+
+    fallback() external payable { }
 }
--- contracts/test/SwapBriber.ts
+++ contracts/test/SwapBriber.ts
@@ -1,50 +1,108 @@
 import hre, { ethers } from 'hardhat';
 const { isAddress, parseUnits } = ethers.utils;
 import { Artifact } from 'hardhat/types';
-// import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
 
-// import { Signers } from '../types';
-import { SwapBriber, TestToken } from '../typechain';
+//import { SwapBriber, TestToken, MockWeth, IUniswapV2Factory, IUniswapV2Router02 } from '../typechain';
+import { SwapBriber, TestToken, MockWeth } from '../typechain';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { expect } from 'chai';
-import { IUniswapV2Factory } from '../typechain/IUniswapV2Factory';
+import { parseEther } from '@ethersproject/units';
+import { Contract } from "@ethersproject/contracts";
+
 
 const { deployContract } = hre.waffle;
 
 // Import Uniswap contract artifacts from their package
 const UniswapV2FactoryArtifact = require('@uniswap/v2-core/build/UniswapV2Factory.json');
 // const UniswapV2PairArtifact = require('@uniswap/v2-core/build/UniswapV2Pair.json');
-// const UniswapV2Router02Artifact = require('@uniswap/v2-periphery/build/UniswapV2Router02.json');
+const UniswapV2Router02Artifact = require('@uniswap/v2-periphery/build/UniswapV2Router02.json');
+
+const farFuture = '2000000000'; // Unix timestamp well into the future
 
 describe('SwapBriber', () => {
   let admin: SignerWithAddress;
   let sender: SignerWithAddress;
+  let lp: SignerWithAddress;
   let token: TestToken;
   let briber: SwapBriber;
-  let uniswapFactory: IUniswapV2Factory;
+  let weth: MockWeth;
+  // let uniswapFactory: IUniswapV2Factory;
+  // let uniswapRouter: IUniswapV2Router02;
+  let uniswapFactory: Contract;
+  let uniswapRouter: Contract;
 
-  const bribeTokens = parseUnits('1000', 18);
+  // The liquidity to be provided to the token/ETH pool
+  const lpTokens = parseUnits('2500', 18);
+  const lpETH = parseEther('5000');
+  const bribeTokens = parseUnits('0.5', 18);
+
+  // Token price denominated in ETH based on pool ratio
+  const tokenPrice = lpETH.div(lpTokens);
+
+  // The bribe ETH is the is the amount of ETH the user can buy with their tokens,
+  // allowing for up to 5% slippage
+  const bribeEth = tokenPrice.mul(bribeTokens).div(100).mul(95);
 
   before(async () => {
-    [ admin, sender ] = await hre.ethers.getSigners();
+    [admin, sender, lp] = await hre.ethers.getSigners();
   });
 
   it('should deploy', async () => {
-    const testTokenArtifact: Artifact = await hre.artifacts.readArtifact('TestToken');
-    token = await deployContract(admin, testTokenArtifact, ['Test Token', 'TT']) as TestToken;
-    expect(isAddress(token.address), 'Failed to deploy TestToken');
-
     const swapBriberArtifact: Artifact = await hre.artifacts.readArtifact('SwapBriber');
-    briber = await deployContract(admin, swapBriberArtifact, []) as SwapBriber;
-    expect(isAddress(briber.address), 'Failed to deploy SwapBriber');
+    briber = (await deployContract(admin, swapBriberArtifact, [])) as SwapBriber;
+    expect(isAddress(briber.address), 'Failed to deploy SwapBriber').to.be.true;
 
-    uniswapFactory = await deployContract(admin, UniswapV2FactoryArtifact, [admin.address]) as IUniswapV2Factory;
-    expect(isAddress(uniswapFactory.address), 'Failed to deploy Uniswap Factory');
+    const testTokenArtifact: Artifact = await hre.artifacts.readArtifact('TestToken');
+    token = (await deployContract(admin, testTokenArtifact, ['Test Token', 'TT'])) as TestToken;
+    expect(isAddress(token.address), 'Failed to deploy TestToken').to.be.true;
+
+    const mockWethArtifact: Artifact = await hre.artifacts.readArtifact('MockWETH');
+    weth = (await deployContract(admin, mockWethArtifact, [])) as MockWeth;
+    expect(isAddress(weth.address), 'Failed to deploy MockWETH').to.be.true;
+
+    uniswapFactory = (await deployContract(admin, UniswapV2FactoryArtifact, [admin.address]));
+    expect(isAddress(uniswapFactory.address), 'Failed to deploy Uniswap Factory').to.be.true;
+
+    uniswapRouter = (await deployContract(admin, UniswapV2Router02Artifact, [
+      uniswapFactory.address,
+      weth.address,
+    ]));
+    expect(isAddress(uniswapRouter.address), 'Failed to deploy Uniswap Router').to.be.true;
   });
 
   it('should mint tokens', async () => {
+    await token.mint(lp.address, lpTokens);
+    const lpBalance = await token.balanceOf(lp.address);
+    expect(lpBalance.eq(lpTokens), 'Failed to mint LP tokens').to.be.true;
+
     await token.mint(sender.address, bribeTokens);
-    const balance = await token.balanceOf(sender.address);
-    expect(balance.eq(bribeTokens), 'Failed to mint tokens');
+    const senderBalance = await token.balanceOf(sender.address);
+    expect(senderBalance.eq(bribeTokens), 'Failed to mint tokens').to.be.true;
+  });
+
+  it('should add liquidity', async () => {
+    await token.connect(lp).approve(uniswapRouter.address, lpTokens);
+
+    await uniswapRouter
+      .connect(lp)
+      .addLiquidityETH(token.address, lpTokens, lpTokens, lpETH, lp.address, farFuture, { value: lpETH });
+  });
+
+  it('should do a swap bribe', async () => {
+    await token.connect(sender).approve(briber.address, bribeTokens);
+
+    await briber
+      .connect(sender)
+      .swapAndBribe(
+        token.address,
+        bribeTokens,
+        bribeEth,
+        uniswapRouter.address,
+        [token.address, weth.address],
+        farFuture,
+      );
+
+    const briberBalance = await token.balanceOf(briber.address);
+    expect(briberBalance.eq(0), 'Failed to trade tokens').to.be.true;
   });
 });
