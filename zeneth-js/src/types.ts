2d7dbcbf1914cafe7b57c5f1295d98883f216cf2
Matt Solomon <matt@mattsolomon.dev>
2021-05-04 13:24:48 -0700

    Add helper methods to help populate transactions before signing

--- zeneth-js/src/types.ts
+++ zeneth-js/src/types.ts
@@ -1,4 +1,12 @@
 // Type definitions
+import { BigNumberish } from '@ethersproject/bignumber';
 import { FlashbotsBundleRawTransaction, FlashbotsBundleTransaction } from '@flashbots/ethers-provider-bundle';
 
 export type FlashbotsTransaction = FlashbotsBundleRawTransaction | FlashbotsBundleTransaction;
+
+export interface TransactionFragment {
+  data: string;
+  gasLimit: BigNumberish;
+  to: string;
+  value: BigNumberish;
+}
