13928b43b4ce0e6f70c2ac6f5e31aced35bff881
Ben DiFrancesco <ben@scopelift.co>
2021-05-05 07:08:59 -0400

    Implement base sweepable storage & update rights

--- contracts/contracts/BaseSweepable.sol
+++ contracts/contracts/BaseSweepable.sol
@@ -11,7 +11,23 @@ contract BaseSweepable is Ownable {
   event SetSweeper(address indexed oldSweeper, address indexed newReceiver);
 
   constructor(address payable _sweepReceiver, address _sweeper) {
+    require(_sweepReceiver != address(0) && _sweeper != address(0), "BaseSweepable: Invalid 0-Addr deploy param");
     sweepReceiver = _sweepReceiver;
     sweeper = _sweeper;
+  }
+
+  function setSweepReceiver(address payable _newReceiver) public onlyOwner nonZero(_newReceiver) {
+    emit SetSweepReceiver(sweepReceiver, _newReceiver);
+    sweepReceiver = _newReceiver;
+  }
+
+  function setSweeper(address _newSweeper) public onlyOwner nonZero(_newSweeper) {
+    emit SetSweeper(sweeper, _newSweeper);
+    sweeper = _newSweeper;
+  }
+
+  modifier nonZero(address _addr) {
+    require(_addr != address(0), "BaseSweepable: Invalid 0-Addr");
+    _;
   }
 }
--- contracts/test/BaseSweepable.ts
+++ contracts/test/BaseSweepable.ts
@@ -1,15 +1,18 @@
 import hre, { ethers } from 'hardhat';
 const { isAddress } = ethers.utils;
+const { AddressZero } = ethers.constants;
 import { Artifact } from 'hardhat/types';
-import { expect } from 'chai';
+import { expect, use } from 'chai';
 
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-const { deployContract } = hre.waffle;
+const { deployContract, solidity } = hre.waffle;
+use(solidity);
 
 import { BaseSweepable } from '../typechain';
 
 describe('BaseSweepable', () => {
   let owner: SignerWithAddress;
+  let other: SignerWithAddress;
   let receiver1: SignerWithAddress;
   let receiver2: SignerWithAddress;
   let sweeper1: SignerWithAddress;
@@ -17,9 +20,7 @@ describe('BaseSweepable', () => {
   let sweepable: BaseSweepable;
 
   before(async () => {
-    [owner, receiver1, receiver2, sweeper1, sweeper2] = await hre.ethers.getSigners();
-    sweeper2;
-    receiver2;
+    [owner, other, receiver1, receiver2, sweeper1, sweeper2] = await hre.ethers.getSigners();
   });
 
   beforeEach(async () => {
@@ -39,5 +40,39 @@ describe('BaseSweepable', () => {
     expect(_owner).to.equal(owner.address);
     expect(sweepReceiver).to.equal(receiver1.address);
     expect(sweeper).to.equal(sweeper1.address);
+  });
+
+  it('should not let a non-owner updated the receiver', async () => {
+    const txp = sweepable.connect(other).setSweepReceiver(receiver2.address);
+    await expect(txp).to.be.revertedWith('Ownable: caller is not the owner');
+  });
+
+  it('should not let a non-owner update the sweeper', async () => {
+    const txp = sweepable.connect(other).setSweeper(sweeper2.address);
+    await expect(txp).to.be.revertedWith('Ownable: caller is not the owner');
+  });
+
+  it('should not let the owner set the receiver to the 0-address', async () => {
+    const txp = sweepable.connect(owner).setSweepReceiver(AddressZero);
+    await expect(txp).to.be.revertedWith('BaseSweepable: Invalid 0-Addr');
+  });
+
+  it('should not let the owner set the sweeper to the 0-address', async () => {
+    const txp = sweepable.connect(owner).setSweeper(AddressZero);
+    await expect(txp).to.be.revertedWith('BaseSweepable: Invalid 0-Addr');
+  });
+
+  it('should let the owner update the receiver', async () => {
+    const txp = sweepable.connect(owner).setSweepReceiver(receiver2.address);
+    await expect(txp).to.emit(sweepable, 'SetSweepReceiver').withArgs(receiver1.address, receiver2.address);
+
+    expect(await sweepable.sweepReceiver()).to.equal(receiver2.address);
+  });
+
+  it('should let the owner update the sweeper', async () => {
+    const txp = sweepable.connect(owner).setSweeper(sweeper2.address);
+    await expect(txp).to.emit(sweepable, 'SetSweeper').withArgs(sweeper1.address, sweeper2.address);
+
+    expect(await sweepable.sweeper()).to.equal(sweeper2.address);
   });
 });
