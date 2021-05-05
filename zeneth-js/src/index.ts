be088e323a00872530a7d8b3447a310156551d37
Matt Solomon <matt@mattsolomon.dev>
2021-05-05 07:09:22 -0700

    Start adding bundle signing method to zeneth-js

--- zeneth-js/src/index.ts
+++ zeneth-js/src/index.ts
@@ -1,10 +1,12 @@
 import { resolve } from 'path';
 import { config as dotenvConfig } from 'dotenv';
+import { getAddress } from '@ethersproject/address';
 import { Zero } from '@ethersproject/constants';
-import { Wallet } from '@ethersproject/wallet';
 import { JsonRpcProvider, TransactionRequest } from '@ethersproject/providers';
+import { serialize, UnsignedTransaction } from '@ethersproject/transactions';
+import { Wallet } from '@ethersproject/wallet';
 import { FlashbotsBundleProvider, FlashbotsOptions } from '@flashbots/ethers-provider-bundle';
-import { FlashbotsTransaction, TransactionFragment } from './types';
+import { TransactionFragment } from './types';
 
 dotenvConfig({ path: resolve(__dirname, '../../.env') });
 
@@ -38,16 +40,84 @@ export class ZenethRelayer {
   }
 
   /**
+   * @notice Generates the transaction object to sign
+   * @dev This function does not try to estimate gas usage because gas estimation will fail if you use a gas price
+   * of zero, and we can't use a gas price of 1 wei because we assume the sender has no ETH, so gas estimation will
+   * fail in that case too
+   * @param tx A TransactionRequest that is not completed. Only from, to, and gasLimit are required
+   */
+  async populateTransaction(tx: TransactionRequest): Promise<TransactionRequest> {
+    const { nonce, from, to, gasLimit } = tx;
+    if (!nonce && !from) throw new Error("Must include either 'from' or 'nonce' field");
+    if (!to) throw new Error("Must include 'to' field");
+    if (!gasLimit) throw new Error("Must include 'gasLimit' field");
+
+    // Return transaction with chain ID and zero gas price
+    return {
+      chainId: this.provider.network.chainId,
+      data: tx.data || '0x',
+      gasLimit,
+      gasPrice: Zero,
+      nonce: nonce || (await this.provider.getTransactionCount(from as string)),
+      to: getAddress(to),
+      value: tx.value || Zero,
+    };
+  }
+
+  /**
+   * @notice Populates multiple transactions that are ready to be signed
+   * @dev Only for use case where all transactions are sent from the same user
+   * @dev This handles nonces as well, and will start with the latest nonce found in the network
+   * @param from The user sending the transactions
+   * @param txs Array of objects containing the data, gasLimit, to, and value for each transaction
+   */
+  async populateTransactions(from: string, txs: TransactionFragment[]): Promise<TransactionRequest[]> {
+    const initialNonce = await this.provider.getTransactionCount(from);
+    const promises = txs.map((tx, index) =>
+      this.populateTransaction({
+        data: tx.data,
+        gasLimit: tx.gasLimit,
+        nonce: initialNonce + index, // increment nonce for each transaction
+        to: tx.to,
+        value: tx.value,
+      })
+    );
+    return Promise.all(promises);
+  }
+
+  /**
+   * @notice Prompts the user to sign transactions use eth_sign
+   * @dev We can't use eth_signTransaction because many wallets do not support it
+   * @dev The user will receive one signature prompt for each transaction fragment
+   * @param from The user sending the transactions
+   * @param txs Array of objects containing the data, gasLimit, to, and value for each transaction
+   */
+  async signBundle(from: string, txs: TransactionFragment[]) {
+    // Get our transactions to sign
+    const populatedTransactions = await this.populateTransactions(from, txs);
+
+    // Prompt the user to sign each and return the signatures
+    // TODO finish and test this
+    const promises = populatedTransactions.map((tx) => {
+      const serializedTx = serialize(tx as UnsignedTransaction);
+      // TODO fallback to personal_sign if eth_sign is not supported?
+      return this.provider.send('eth_sign', [serializedTx]);
+    });
+    return Promise.all(promises) as Promise<string[]>;
+  }
+
+  /**
    * @notice Sends a flashbots bundle
-   * @param txs Array of flashbots transactions to send
+   * @param txs Array of signed transactions to send
    * @param validBlocks Starting from the next block, how many total blocks are valid for mining the transactions. A
    * value of 1 means only the next block is valid, a value of 6 means the next 6 blocks are valid
    */
-  async sendBundle(txs: FlashbotsTransaction[], validBlocks: number, opts?: FlashbotsOptions) {
+  async sendBundle(txs: string[], validBlocks: number, opts?: FlashbotsOptions) {
     if (validBlocks < 1) throw new Error(`Minimum 'validBlocks' value is 1, got ${validBlocks}`);
 
     // First we sign the bundle of transactions with our auth key
-    const signedBundle = await this.flashbotsProvider.signBundle(txs);
+    const flashbotsTxs = txs.map((tx) => ({ signedTransaction: tx }));
+    const signedBundle = await this.flashbotsProvider.signBundle(flashbotsTxs);
 
     // Next we define the targetBlockNumbers, which are "The only block number for which the bundle is to be considered
     // valid. If you would like more than one block to be targeted, submit multiple rpc calls targeting each specific
@@ -68,56 +138,6 @@ export class ZenethRelayer {
 
     // No errors simulating, so send the bundle
     return targetBlocks.map((block) => this.flashbotsProvider.sendRawBundle(signedBundle, block, opts));
-  }
-
-  /**
-   * @notice Generates the transaction object to sign
-   * @dev This function does not try to estimate gas usage because gas estimation will fail if you use a gas price
-   * of zero, and we can't use a gas price of 1 wei because we assume the sender has no ETH, so gas estimation will
-   * fail in that case too
-   */
-  async populateTransaction(tx: TransactionRequest): Promise<TransactionRequest> {
-    const { from, to, gasLimit } = tx;
-    if (!from) throw new Error("Must include 'from' field");
-    if (!to) throw new Error("Must include 'to' field");
-    if (!gasLimit) throw new Error("Must include 'gasLimit' field");
-
-    // Check if user provided values, otherwise fallback to the defaults
-    const nonce = tx.nonce || (await this.provider.getTransactionCount(from));
-    const value = tx.value || Zero;
-    const data = tx.data || '0x';
-
-    // Return transaction with chain ID and zero gas price
-    return {
-      chainId: this.provider.network.chainId,
-      data,
-      from,
-      gasLimit,
-      gasPrice: Zero,
-      nonce,
-      to,
-      value,
-    };
-  }
-
-  /**
-   * @notice Populates multiple transactions that are ready to be signed
-   * @dev Only for use case where all transactions are sent from the same user
-   * @dev This handles nonces as well, and will start with the latest nonce found in the network
-   */
-  async populateTransactions(from: string, txs: TransactionFragment[]): Promise<TransactionRequest[]> {
-    const initialNonce = await this.provider.getTransactionCount(from);
-    const promises = txs.map((tx, index) =>
-      this.populateTransaction({
-        data: tx.data,
-        from,
-        gasLimit: tx.gasLimit,
-        nonce: initialNonce + index, // increment nonce for each transaction
-        to: tx.to,
-        value: tx.value,
-      })
-    );
-    return Promise.all(promises);
   }
 }
 
