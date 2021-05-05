a8768ab350adba778d16b593aa70165849613aec
Ben DiFrancesco <ben@scopelift.co>
2021-05-05 11:43:31 -0400

    Implement and test ETH sweeping

--- contracts/contracts/EthSweepable.sol
+++ contracts/contracts/EthSweepable.sol
@@ -4,9 +4,14 @@ pragma solidity ^0.8.4;
 import "./BaseSweepable.sol";
 
 contract EthSweepable is BaseSweepable {
-  event SweptEth();
+  event SweptEth(address indexed receiver);
 
   constructor(address payable _sweepReceiver, address _sweeper) BaseSweepable(_sweepReceiver, _sweeper) {}
 
-  function sweepEth() public onlySweeper {}
+  function sweepEth() public onlySweeper {
+    emit SweptEth(sweepReceiver);
+    sweepReceiver.transfer(address(this).balance);
+  }
+
+  receive() external payable {}
 }
--- contracts/test/EthSweepable.ts
+++ contracts/test/EthSweepable.ts
@@ -1,5 +1,6 @@
 import hre, { ethers } from 'hardhat';
-const { isAddress } = ethers.utils;
+const { provider } = ethers;
+const { isAddress, parseEther } = ethers.utils;
 const { AddressZero } = ethers.constants;
 import { Artifact } from 'hardhat/types';
 import { expect, use } from 'chai';
@@ -17,6 +18,8 @@ describe('EthSweepable', () => {
   let sweeper: SignerWithAddress;
   let sweepable: EthSweepable;
 
+  const sendAmount = parseEther('0.361');
+
   before(async () => {
     [owner, other, receiver, sweeper] = await hre.ethers.getSigners();
     AddressZero;
@@ -28,17 +31,36 @@ describe('EthSweepable', () => {
     expect(isAddress(sweepable.address), 'Failed to deploy EthSweepable').to.be.true;
 
     // Empty the receiver's balance
-    let receiverBalance = await ethers.provider.getBalance(receiver.address);
+    let receiverBalance = await provider.getBalance(receiver.address);
     await receiver.sendTransaction({
       to: owner.address,
       value: receiverBalance,
     });
-    receiverBalance = await ethers.provider.getBalance(receiver.address);
+    receiverBalance = await provider.getBalance(receiver.address);
     expect(receiverBalance.eq(0), 'Failed to empty receiver balance').to.be.true;
   });
 
   it('should not let a non-sweeper sweep', async () => {
     const txp = sweepable.connect(other).sweepEth();
     await expect(txp).to.be.revertedWith('BaseSweepable: Not Sweeper');
+  });
+
+  it('should send to the receiver when swept', async () => {
+    await other.sendTransaction({
+      to: sweepable.address,
+      value: sendAmount,
+    });
+
+    let sweepableBalance = await provider.getBalance(sweepable.address);
+    expect(sweepableBalance.eq(sendAmount), 'Failed to send to sweepable').to.be.true;
+
+    const txp = sweepable.connect(sweeper).sweepEth();
+    await expect(txp).to.emit(sweepable, 'SweptEth').withArgs(receiver.address);
+
+    sweepableBalance = await provider.getBalance(sweepable.address);
+    expect(sweepableBalance.eq(0), 'Failed to sweep').to.be.true;
+
+    const receiverBalance = await provider.getBalance(receiver.address);
+    expect(receiverBalance.eq(sendAmount), 'Failed to sweep to receiver').to.be.true;
   });
 });
