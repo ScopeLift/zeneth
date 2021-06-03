dbc6804159a0617a3f1289c2874c036f34ccf4d7
wildmolasses <changes@gmail.com>
2021-06-03 11:27:56 -0500

    make l2 coming soon

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,12 +2,15 @@ import Link from 'next/link';
 
 const Index = () => {
   const buttonClass =
-    'mt-10 p-5 cursor-pointer text-xl bg-gradient-to-br from-red-300 to-purple-400 rounded opacity-90 hover:opacity-100';
+    'mt-10 p-5 py-7 flex self-center align-middle cursor-pointer text-xl bg-gradient-to-br from-red-300 to-purple-400 rounded opacity-90 hover:opacity-100';
   return (
     <div className="flex flex-row">
-      <Link href="/l2-entry">
-        <div className={`${buttonClass} mr-3`}>Gasless L2 Entry</div>
-      </Link>
+      {/* <Link href="/l2-entry"> */}
+      <div className="mt-10 p-5 cursor-default rounded bg-gray-200 text-gray-600 text-xl mr-3 flex flex-col align-middle">
+        Gasless L2 Entry <br />
+        <span className="text-sm text-center block">(Coming soon)</span>
+      </div>
+      {/* </Link> */}
       <Link href="/token-transfer">
         <div className={buttonClass}>Gasless Token Transfer</div>
       </Link>
