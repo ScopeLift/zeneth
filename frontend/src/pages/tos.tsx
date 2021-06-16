cb0e9ab5a023aaac9d63a9d9d213ff5127cc5695
Ben DiFrancesco <ben@scopelift.co>
2021-06-16 09:57:16 -0400

    Add privacy policy via static PDF linked in footer

---
+++ frontend/src/pages/tos.tsx
@@ -0,0 +1,129 @@
+const TOS = () => {
+  return (
+    <div>
+      <p>
+        <a name="collect"></a>
+      </p>
+      <h2>
+        <a href="#collect">What information do we collect?</a>
+      </h2>
+      <p>We may retain server logs which include the IP address of every request to our server.</p>
+      <p>
+        <a name="use"></a>
+      </p>
+      <h2>
+        <a href="#use">What do we use your information for?</a>
+      </h2>
+      <p>Any of the information we collect from you may be used in one of the following ways:</p>
+      <ul>
+        <li>To personalize your experience — your information helps us to better respond to your individual needs.</li>
+        <li>
+          To improve our site — we continually strive to improve our site offerings based on the information and
+          feedback we receive from you.
+        </li>
+        <li>
+          To improve customer service — your information helps us to more effectively respond to your customer service
+          requests and support needs.
+        </li>
+      </ul>
+      <p>
+        <a name="protect"></a>
+      </p>
+      <h2>
+        <a href="#protect">How do we protect your information?</a>
+      </h2>
+      <p>
+        We implement a variety of security measures to maintain the safety of your personal information when you enter,
+        submit, or access your personal information.
+      </p>
+      <p>
+        <a name="data-retention"></a>
+      </p>
+      <h2>
+        <a href="#data-retention">What is your data retention policy?</a>
+      </h2>
+      <p>We will make a good faith effort to:</p>
+      <ul>
+        <li>Retain server logs containing the IP address of all requests to this server no more than 90 days.</li>
+        <li>Retain the IP addresses associated with registered users and their posts no more than 5 years.</li>
+      </ul>
+      <p>
+        <a name="cookies"></a>
+      </p>
+      <h2>
+        <a href="#cookies">Do we use cookies?</a>
+      </h2>
+      <p>
+        We do not use cookies. We do use local browser storage to store user preferences and settings between sessions.
+        This data is never used to track users, and not sent anywhere beyond the user’s local device.
+      </p>
+      <p>
+        <a name="disclose"></a>
+      </p>
+      <h2>
+        <a href="#disclose">Do we disclose any information to outside parties?</a>
+      </h2>
+      <p>
+        We do not sell, trade, or otherwise transfer to outside parties your personally identifiable information. This
+        does not include trusted third parties who assist us in operating our site, conducting our business, or
+        servicing you, so long as those parties agree to keep this information confidential. We may also release your
+        information when we believe release is appropriate to comply with the law, enforce our site policies, or protect
+        ours or others rights, property, or safety. However, non-personally identifiable visitor information may be
+        provided to other parties for marketing, advertising, or other uses.
+      </p>
+      <p>
+        <a name="third-party"></a>
+      </p>
+      <h2>
+        <a href="#third-party">Third party links</a>
+      </h2>
+      <p>
+        Occasionally, at our discretion, we may include or offer third party products or services on our site. These
+        third party sites have separate and independent privacy policies. We therefore have no responsibility or
+        liability for the content and activities of these linked sites. Nonetheless, we seek to protect the integrity of
+        our site and welcome any feedback about these sites.
+      </p>
+      <p>
+        <a name="coppa"></a>
+      </p>
+      <h2>
+        <a href="#coppa">Children’s Online Privacy Protection Act Compliance</a>
+      </h2>
+      <p>
+        Our site, products and services are all directed to people who are at least 13 years old or older. If this
+        server is in the USA, and you are under the age of 13, per the requirements of COPPA (
+        <a href="https://en.wikipedia.org/wiki/Children%27s_Online_Privacy_Protection_Act">
+          Children’s Online Privacy Protection Act
+        </a>
+        ), do not use this site.
+      </p>
+      <p>
+        <a name="online"></a>
+      </p>
+      <h2>
+        <a href="#online">Online Privacy Policy Only</a>
+      </h2>
+      <p>
+        This online privacy policy applies only to information collected through our site and not to information
+        collected offline.
+      </p>
+      <p>
+        <a name="consent"></a>
+      </p>
+      <h2>
+        <a href="#consent">Your Consent</a>
+      </h2>
+      <p>By using our site, you consent to our web site privacy policy.</p>
+      <p>
+        <a name="changes"></a>
+      </p>
+      <h2>
+        <a href="#changes">Changes to our Privacy Policy</a>
+      </h2>
+      <p>If we decide to change our privacy policy, we will post those changes on this page.</p>
+      <p>This document is CC-BY-SA. It was last updated June 16, 2021.</p>
+    </div>
+  );
+};
+
+export default TOS;
