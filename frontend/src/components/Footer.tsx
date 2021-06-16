3ba6ca4566d055b03f19cb04c1a8b5bf3372a230
Ben DiFrancesco <ben@scopelift.co>
2021-06-16 09:57:16 -0400

    Add privacy policy via static PDF linked in footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,6 +4,14 @@ export const Footer = () => {
       Created by{' '}
       <a href="https://scopelift.co" rel="noreferrer" target="_blank" className="text-purple-800">
         ScopeLift
+      </a>{' '}
+      |{' '}
+      <a href="/static/docs/Zeneth-Privacy-Policy-061621.pdf" target="_blankd" className="text-purple-800">
+        Privacy Policy
+      </a>{' '}
+      |{' '}
+      <a href="/static/docs/Zeneth-Privacy-Policy-061621.pdf" target="_blankd" className="text-purple-800">
+        Terms of Service
       </a>
     </div>
   );
