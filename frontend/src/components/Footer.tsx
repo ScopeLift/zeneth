592c528bbf6b041c4de094d3a2b90fbc299366a3
Ben DiFrancesco <ben@scopelift.co>
2021-06-16 22:21:58 -0400

    Add support email and icon attribution to footer
    * Add support email and icon attribution to footer
    * Tonight we're gonna HTML like it's 1999

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,18 +1,36 @@
 export const Footer = () => {
   return (
     <div className="mb-4">
-      Created by{' '}
-      <a href="https://scopelift.co" rel="noreferrer" target="_blank" className="text-purple-800">
-        ScopeLift
-      </a>{' '}
-      |{' '}
+      <center>
+        Created by{' '}
+        <a href="https://scopelift.co" rel="noreferrer" target="_blank" className="text-purple-800">
+          ScopeLift
+        </a>
+      </center>
       <a href="/static/docs/Zeneth-Privacy-Policy-061621.pdf" target="_blankd" className="text-purple-800">
         Privacy Policy
       </a>{' '}
       |{' '}
       <a href="/static/docs/Zeneth-Terms-of-Service-061621.pdf" target="_blankd" className="text-purple-800">
         Terms of Service
+      </a>{' '}
+      |{' '}
+      <a href="mailto:support@zeneth.app" className="text-purple-800">
+        support@zeneth.app
       </a>
+      <center>
+        <small>
+          Icon by{' '}
+          <a
+            href="https://www.flaticon.com/authors/good-ware"
+            target="_blankd"
+            className="text-purple-800"
+            title="Good Ware"
+          >
+            Good Ware
+          </a>
+        </small>
+      </center>
     </div>
   );
 };
