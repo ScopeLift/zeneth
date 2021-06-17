c4691091883ebfd4050ef9731188bd76a85ff1b5
John Feras <jferas@ferasinfotech.com>
2021-06-17 13:33:29 -0400

    Made expected values in test less 'magic' and added test for eth bribe value

--- zeneth-js/test/fee-estimation.test.ts
+++ zeneth-js/test/fee-estimation.test.ts
@@ -13,16 +13,34 @@ import { estimateFee } from '../src';
 
 describe('Estimates Fees', () => {
   it('does a very simple fee calculation', async () => {
-    sinon.stub(helpers, 'getGasPrice').returns(Promise.resolve(BigNumber.from(2e10)));
-    sinon.stub(helpers, 'getTokenPriceInUsd').returns(Promise.resolve(1));
-    sinon.stub(helpers, 'getEthPriceInUsd').returns(Promise.resolve(2000));
+    // setup test gas limit value for bundle and mocked values from web API calls
+    const bundleGasLimit = 300000;
+    const mockedGasPrice = 2e10;
+    const mockedTokenPrice = 1;
+    const mockedEthPrice = 2000;
+    sinon.stub(helpers, 'getGasPrice').returns(Promise.resolve(BigNumber.from(mockedGasPrice)));
+    sinon.stub(helpers, 'getTokenPriceInUsd').returns(Promise.resolve(mockedTokenPrice));
+    sinon.stub(helpers, 'getEthPriceInUsd').returns(Promise.resolve(mockedEthPrice));
+
+    // calculated expected estimates for tokens and eth
+    const expectedEthPriceForBundle = BigNumber.from(bundleGasLimit).mul(BigNumber.from(mockedGasPrice)).mul(2);
+    const expectedBundlePriceInUsd = expectedEthPriceForBundle.mul(BigNumber.from(mockedEthPrice));
+    const expectedTokensNeededForBribe = expectedBundlePriceInUsd.div(BigNumber.from(mockedTokenPrice));
+    const humanReadableExpectedFeeForEth = Number(formatUnits(expectedEthPriceForBundle, 18));
+    const humanReadableExpectedFeeForTokens = Number(formatUnits(expectedTokensNeededForBribe, 18));
+
     const estimate = await estimateFee({
       tokenAddress: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
       tokenDecimals: 18,
-      bundleGasLimit: [50000, 100000, 150000].reduce((x, y) => x + y),
+      bundleGasLimit: bundleGasLimit,
       flashbotsPremiumMultiplier: 2,
     });
-    const humanFee = Number(formatUnits(estimate.bribeInTokens, 18));
-    expect(humanFee).to.equal(24);
+
+    // convert result from bignumbers to human-readable
+    const humanReadableFeeForTokens = Number(formatUnits(estimate.bribeInTokens, 18));
+    const humanReadableFeeForEth = Number(estimate.bribeInEth);
+
+    expect(humanReadableFeeForEth).to.equal(humanReadableExpectedFeeForEth);
+    expect(humanReadableFeeForTokens).to.equal(humanReadableExpectedFeeForTokens);
   });
 });
