e11f193010286fa4430e3c1551ed73e485a13912
John Feras <jferas@ferasinfotech.com>
2021-06-22 11:34:29 -0400

    Fixed an simplified estimateFee test and remove magic number for flashbotPremium

--- zeneth-js/test/fee-estimation.test.ts
+++ zeneth-js/test/fee-estimation.test.ts
@@ -1,4 +1,3 @@
-import { formatUnits } from '@ethersproject/units';
 import { expect } from 'chai';
 import fetch from 'node-fetch';
 
@@ -18,29 +17,26 @@ describe('Estimates Fees', () => {
     const mockedGasPrice = 2e10;
     const mockedTokenPrice = 1;
     const mockedEthPrice = 2000;
+    const flashbotsPremium = 2;
     sinon.stub(helpers, 'getGasPrice').returns(Promise.resolve(BigNumber.from(mockedGasPrice)));
     sinon.stub(helpers, 'getTokenPriceInUsd').returns(Promise.resolve(mockedTokenPrice));
     sinon.stub(helpers, 'getEthPriceInUsd').returns(Promise.resolve(mockedEthPrice));
 
     // calculated expected estimates for tokens and eth
-    const expectedEthPriceForBundle = BigNumber.from(bundleGasLimit).mul(BigNumber.from(mockedGasPrice)).mul(2);
+    const expectedEthPriceForBundle = BigNumber.from(bundleGasLimit)
+      .mul(BigNumber.from(mockedGasPrice))
+      .mul(flashbotsPremium);
     const expectedBundlePriceInUsd = expectedEthPriceForBundle.mul(BigNumber.from(mockedEthPrice));
     const expectedTokensNeededForBribe = expectedBundlePriceInUsd.div(BigNumber.from(mockedTokenPrice));
-    const humanReadableExpectedFeeForEth = Number(formatUnits(expectedEthPriceForBundle, 18));
-    const humanReadableExpectedFeeForTokens = Number(formatUnits(expectedTokensNeededForBribe, 18));
 
     const estimate = await estimateFee({
       tokenAddress: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
       tokenDecimals: 18,
       bundleGasLimit: bundleGasLimit,
-      flashbotsPremiumMultiplier: 2,
+      flashbotsPremiumMultiplier: flashbotsPremium,
     });
 
-    // convert result from bignumbers to human-readable
-    const humanReadableFeeForTokens = Number(formatUnits(estimate.bribeInTokens, 18));
-    const humanReadableFeeForEth = Number(estimate.bribeInEth);
-
-    expect(humanReadableFeeForEth).to.equal(humanReadableExpectedFeeForEth);
-    expect(humanReadableFeeForTokens).to.equal(humanReadableExpectedFeeForTokens);
+    expect(estimate.bribeInEth).to.equal(expectedEthPriceForBundle);
+    expect(estimate.bribeInTokens).to.equal(expectedTokensNeededForBribe);
   });
 });
